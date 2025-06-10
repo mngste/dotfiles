19312a295291816ba8af70208d30bf2c34f4cc29
mngt <80961204+mngste@users.noreply.github.com>
2025-06-10 06:37:22 +0200

    Create config.fish

---
+++ config/fish/.config/fish/config.fish
@@ -0,0 +1,36 @@
+# Alias
+alias cr "composer require "
+alias sy "php bin/console "
+alias mdata "sudo mkdir -p /run/media/mngt/Data && sudo ntfs-3g /dev/sda2 /run/media/mgt/Data"
+alias remove "pacaur -Rns"
+alias ls 'eza --icons=auto' # ls replacement
+alias ll 'eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias upgrade 'yay -Syu --noconfirm && flatpak update' # Upgrade the system
+alias fserve 'caddy file-server --listen :8000 --browse' # Serve the current directory
+alias webcam 'mpv av://v4l2:/dev/video0 --profile=low-latency --untimed' # Show the webcam
+
+# Docker
+alias dk "docker kill (docker ps -q)"
+alias de "docker-compose exec"
+alias dprune "docker system prune --filter \"until=720h\" && docker volume prune"
+
+# Sail
+alias sail "bash vendor/bin/sail"
+
+# SSH agent
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+
+# PHP
+set -x PATH $PATH $HOME/.config/composer/vendor/bin
+
+# Local bin
+set -gx PATH "$HOME/.local/bin" $PATH
+set -x PATH $PATH $HOME/.bin
+
+# Golang
+set -gx GOPATH "$HOME/go"
+set -gx PATH "$GOPATH/bin" $PATH
+
+# Bun
+set -Ux BUN_INSTALL "/home/mngt/.bun"
+set -px --path PATH "/home/mngt/.bun/bin"
