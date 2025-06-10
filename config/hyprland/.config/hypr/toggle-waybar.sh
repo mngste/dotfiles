f489e9b1b7e2e3baebd71ce8b48e81f2d8b07065
mngt <80961204+mngste@users.noreply.github.com>
2025-06-10 09:36:24 +0200

    Create toggle-waybar.sh

---
+++ config/hyprland/.config/hypr/toggle-waybar.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+
+function restart_waybar
+    # Attempt to kill existing waybar processes
+    if killall waybar
+        hyprctl keyword general:gaps_out 0
+        hyprctl keyword general:gaps_in 1
+        hyprctl keyword general:border_size 1
+        hyprctl keyword animations:enabled false
+    else
+        hyprctl keyword general:gaps_out 4
+        hyprctl keyword general:gaps_in 4
+        hyprctl keyword general:border_size 2
+        hyprctl keyword animations:enabled true
+        waybar &
+    end
+end
+
+# Run the function
+restart_waybar
