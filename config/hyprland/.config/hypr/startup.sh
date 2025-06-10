3462fee6308330a8c9aff506f44eea9e893531cd
mngt <80961204+mngste@users.noreply.github.com>
2025-06-10 09:37:00 +0200

    Create startup.sh

---
+++ config/hyprland/.config/hypr/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+hyprctl dispatch exec [workspace 2 silent] kitty ~
+# hyprctl dispatch workspace 3
+# hyprctl dispatch exec [workspace 3] "flatpak run com.brave.Browser --profile-directory=\"Default\""
+# sleep 1
+# hyprctl dispatch workspace 7
+# hyprctl dispatch exec [workspace 7] "flatpak run com.brave.Browser --profile-directory=\"Profile 1\""
+#sleep 1
+#hyprctl dispatch workspace 1
+#hyprctl dispatch exec [workspace 1] phpstorm
+sleep 1
+hyprctl dispatch workspace 5
+hyprctl dispatch exec [workspace 5] flatpak run com.discordapp.Discord
