833daefb118ddfbdb40b389151896a7bc97f198c
mngt <80961204+mngste@users.noreply.github.com>
2025-06-11 09:19:39 +0200

    Update startup.sh

--- config/hyprland/.config/hypr/startup.sh
+++ config/hyprland/.config/hypr/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# hyprctl dispatch exec [workspace 2 silent] kitty ~
+hyprctl dispatch exec [workspace 1 silent] kitty ~
 # hyprctl dispatch workspace 3
-# hyprctl dispatch exec [workspace 3] "flatpak run com.brave.Browser --profile-directory=\"Default\""
-# sleep 1
+hyprctl dispatch exec [workspace 3] "flatpak run com.brave.Browser --profile-directory=\"Default\""
+sleep 1
 # hyprctl dispatch workspace 7
 # hyprctl dispatch exec [workspace 7] "flatpak run com.brave.Browser --profile-directory=\"Profile 1\""
 #sleep 1
