f1cfc69c2aee76e46f712d5625518a7cc956f55c
mngt <80961204+mngste@users.noreply.github.com>
2025-12-12 14:25:00 +0100

    Update init.lua

--- config/nvim/.config/nvim/init.lua
+++ config/nvim/.config/nvim/init.lua
@@ -1,2 +1,11 @@
 require("options")
 require("plugins")
+
+vim.api.nvim_create_augroup("lsp", { clear = true }),
+        vim.api.nvim_create_autocmd("BufWritePre", { callback = function(args)
+        vim.api.nvim_create_autocmd("BufWritePre", { buffer = args.buf, callback = function()
+        vim.lsp.buf.format {async = false, id = args.data.client_id } end, buffer = args.buf, callback =
+        function() }) vim.lsp.buf.format {async = false, id = args.data.client_id } end end,})
+    })
+  end
+})
