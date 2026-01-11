88ec134077bb2648383facebd8215068f2464ec7
mngste <mangouste@mosaurus.com>
2026-01-11 22:26:30 +0100

    edit run

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
+
+# fish
 sudo dnf install fish
+sudo chsh -s /usr/bin/fish $(whoami)
+
+# alacritty
+cd ~
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+rustup override set stable
+rustup update stable
+sudo dnf install cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++
+infocmp alacritty
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+sudo mkdir -p /usr/local/share/man/man1
+sudo mkdir -p /usr/local/share/man/man5
+scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
+scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
+echo "source $(pwd)/extra/completions/alacritty.bash" >> ~/.bashrc
+mkdir -p $fish_complete_path[1]
+cp ~/alacritty/extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
