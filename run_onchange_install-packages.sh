18f6a17bcf9d8b008b532c0e535c629368a52aff
mngste <mangouste@mosaurus.com>
2026-01-11 22:57:59 +0100

    edit chezmoi

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -4,26 +4,27 @@
 sudo dnf install fish
 sudo chsh -s /usr/bin/fish $(whoami)
 
-# alacritty
-cd ~
-git clone https://github.com/alacritty/alacritty.git
-cd alacritty
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup override set stable
-rustup update stable
-sudo dnf install cmake freetype-devel fontconfig-devel libxcb-devel libxkbcommon-devel g++
-infocmp alacritty
-sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-sudo desktop-file-install extra/linux/Alacritty.desktop
-sudo update-desktop-database
-sudo mkdir -p /usr/local/share/man/man1
-sudo mkdir -p /usr/local/share/man/man5
-scdoc < extra/man/alacritty.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
-scdoc < extra/man/alacritty-msg.1.scd | gzip -c | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
-scdoc < extra/man/alacritty.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty.5.gz > /dev/null
-scdoc < extra/man/alacritty-bindings.5.scd | gzip -c | sudo tee /usr/local/share/man/man5/alacritty-bindings.5.gz > /dev/null
-echo "source $(pwd)/extra/completions/alacritty.bash" >> ~/.bashrc
-mkdir -p $fish_complete_path[1]
-cp ~/alacritty/extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
+# htop btop
+sudo dnf install htop
+sudo dnf install btop
+
+# fzf
+sudo dnf install fzf
+
+# bat
+sudo dnf install bat
+
+# make bat by default
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+
+# neovim
+sudo dnf install neovim
+
+# tmux
+sudo dnf install tmux
+
+# tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "reboot !"
