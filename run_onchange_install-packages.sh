d4b9099d55edc9d4d2f20699dceb6c8dfdcb8106
mngste <mangouste@mosaurus.com>
2026-01-12 10:11:21 +0100

    edit install

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 # fish
-sudo dnf install fish
+sudo dnf install fish -y
 sudo chsh -s /usr/bin/fish $(whoami)
 
 # htop btop
-sudo dnf install htop
-sudo dnf install btop
+sudo dnf install htop -y
+sudo dnf install btop -y
 
 # fzf
-sudo dnf install fzf
+sudo dnf install fzf -y
 
 # bat
-sudo dnf install bat
+sudo dnf install bat -y
 
 # make bat by default
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 
 # neovim
-sudo dnf install neovim
+sudo dnf install neovim -y
 
 # tmux
-sudo dnf install tmux
+sudo dnf install tmux -y
 
 # tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install timeshift
-sudo dnf install timeshift
+sudo dnf install timeshift -y
 
 echo "reboot !"
