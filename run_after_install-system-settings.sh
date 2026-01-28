bbe221fb54c82c60cd0a9cf03f102a52bd982198
mngt <80961204+mngste@users.noreply.github.com>
2026-01-28 09:57:10 +0100

    Fix command to append zsh to /etc/shells

--- run_after_install-system-settings.sh
+++ run_after_install-system-settings.sh
@@ -7,7 +7,7 @@ echo "> Using zsh shell"
 
 # Check if zsh is the default shell
 if [ "$SHELL" != "/bin/zsh" ]; then
-    command -v zsh | sudo tee -a /etc/shells
+    command -v zsh | tee -a /etc/shells
     #sudo chsh -s $(which zsh) $USER
     chsh -s $(which zsh)
     source .zshrc
