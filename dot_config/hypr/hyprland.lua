a1062fa8dc8c0148fec2d0417367a6d8d447cf8c
mngt <80961204+mngste@users.noreply.github.com>
2026-05-12 10:18:18 +0200

    Update hyprland.lua

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -15,12 +15,7 @@
 ------------------
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
-hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
-    scale    = "1",
-})
+require("hyprland/monitors")
 
 
 ---------------------
