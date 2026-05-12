1ea2b4fc373dca6b3e402a8ac9d3c379ae3286ba
mngt <80961204+mngste@users.noreply.github.com>
2026-05-12 10:17:49 +0200

    Create monitors.lua

---
+++ dot_config/hypr/hyprland/monitors.lua
@@ -0,0 +1,11 @@
+-- laptop
+hl.monitor({
+  output = "eDP-1",
+  mode = "1920x1080@60",
+  position = "0x0",
+  scale = 1,
+})
+
+-- desktop 2 screens
+hl.monitor({ output = "DP-1", mode = "1920x1080@60", position = "0x0", scale = 1 })
+hl.monitor({ output = "DP-2", mode = "1920x1080@60", position = "1920x0", scale = 1, transform = 1 })
