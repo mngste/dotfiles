af8240320aeb58db3d5a77057e8339c7559b7aa3
mngt <80961204+mngste@users.noreply.github.com>
2026-05-18 10:11:38 +0200

    Create aliases.zsh

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,88 @@
+# ======= alias system ======= #
+alias today='date +%Y-%m-%d'
+alias update='sudo dnf update && sudo dnf upgrade'
+
+# ======= alias folder ======= #
+alias nconf='cd $HOME/.config/nvim/'
+alias md="mkdir"
+# better ls
+alias ls='eza --icons'
+alias ll='eza -lh --icons --git'
+alias la='eza -lah --icons --git'
+# Tree view
+alias tree='eza --tree --icons'
+# Reuse ls completions for eza (avoids defining a separate completion function)
+compdef eza=ls
+
+# ======= chezmoi ======= #
+alias ch='chezmoi cd'
+# edit the zsh config file
+alias ec='chezmoi edit $HOME/.zshrc'
+
+# ======= files ======= #
+# count files
+alias count='find . -type f | wc -l'
+alias v='nvim'
+alias fv='nvim $(fzfb)'
+# cat
+alias cat='bat --paging never --theme DarkNeon --style plain,changes'
+# fzf
+alias fzfb='fzf --preview "bat --color always --style numbers,changes {}"'
+
+# ======= tmux ======= #
+alias tns='tmux new -s $(echo $(pwd) | xargs basename)'
+
+# ======= docker ======= #
+alias d='docker'
+alias dipru='docker image prune -a'
+alias dps='docker ps'
+alias dvls='docker volume ls'
+alias dcu='docker compose up -d'
+alias dcd='docker compose down'
+
+# ======= kubectl ======= #
+alias k='kubectl'
+alias kl='kubectl logs'
+alias kg='kubectl get $1'
+alias kgy='kubectl get $1 $2 -oyaml'
+alias kn='kubens'
+alias kc='kubectx'
+
+# ======= git ======= #
+alias g='git'
+alias ga='git add'
+alias gaa='git add -A'
+alias gs='git status --short'
+alias gst='git status'
+alias gc='git commit -m'
+alias gca='git commit --amend'
+alias gb='git branch'
+alias gco='git checkout'
+alias gp='git push origin $(git rev-parse --abbrev-ref HEAD)'
+alias gmg='git merge'
+alias gpf='git push --force'
+alias lg='lazygit'
+
+# ======= source ======= #
+alias sc='source $HOME/.zshrc'
+
+# ======= functions ======= #
+# gcap: commit all and push
+gcap() {
+    git commit -a -m $1
+    git push
+}
+
+# gmerge: merge current branch to the given branch ======= #
+gmerge() {
+    branch=$(git rev-parse --abbrev-ref HEAD) && \
+    echo "merging" $branch "to" $1
+    git checkout $1 && \
+    git merge $branch
+}
+
+# make dir and cd into it
+mdcd() {
+  mkdir -p -- "$1" && cd -P -- "$1"
+}
+
